258fcf07f9874e414b75ed202f13cbfc06c14cab
Denmar Ermitano <ermitano.den@gmail.com>
2022-02-11 17:58:36 -0500

    updated timeline, added htaccess files to rewrite to home and protect app folder

--- Assignment1/app/views/Timeline/timeline.php
+++ Assignment1/app/views/Timeline/timeline.php
@@ -1,8 +1,38 @@
-<?php require APPROOT . '/views/includes/header.php'; 
+<?php 
+    require APPROOT . '/views/includes/header.php'; 
 ?>
-
     <h1>Timeline</h1>
     <h2>This is a PHP E-COMMERCE WEB APP</h2>
 
+        <table class="table table-light table-striped">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Last Name</th>
+          <th scope="col">First Name</th>
+          <th scope="col">Student ID</th>
+          <th scope="col">Task</th>
+          <th scope="col">Time Remaining</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Alagos Eli</td>
+          <td>Alvin</td>
+          <td>111111</td>
+          <td>Task</td>
+          <td>Time</td> <! add javascript here !>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Ermitano</td>
+          <td>Denmar Lawrence</td>
+          <td>2082991</td>
+          <td>Task</td>
+          <td>Time</td>
+        </tr>
+      </tbody>
+    </table>
 
 <?php require APPROOT . '/views/includes/footer.php'; ?>
