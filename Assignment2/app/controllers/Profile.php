f65f5c1e9011c56c8c7caa17c1972a556ac6222a
Alvin Alagos Eli <1960943@edu.vaniercollege.qc.ca>
2022-03-10 09:51:34 -0500

    added profile page view

---
+++ Assignment2/app/controllers/Profile.php
@@ -0,0 +1,14 @@
+<?php
+
+class Profile extends Controller{
+
+    public function __construct(){
+
+    }
+
+    public function index(){
+        $this->view('Profile/profilePage');
+    }
+}
+
+?>
---
+++ Assignment2/app/views/Profile/profilePage.php
@@ -0,0 +1,35 @@
+<?php require APPROOT . '/views/includes/header.php'; 
+?>
+
+
+
+
+        <!-- Profile widget -->
+        <div class="bg-white shadow rounded overflow-hidden">
+            <div class="px-4 pt-0 pb-4 cover">
+                <div class="media align-items-end profile-head">
+                    <div class="profile mr-3"><img src="https://cdn.vox-cdn.com/thumbor/CNLDJd5-M-WtOsYh_yugQ-S-FJs=/715x248:1689x721/1400x933/filters:focal(972x299:1278x605):no_upscale()/cdn.vox-cdn.com/uploads/chorus_image/image/69305239/shrek4_disneyscreencaps.com_675.0.jpg" alt="..." width="130" class="rounded mb-2 img-thumbnail">
+            
+                        <a href="#" class="btn btn-outline-dark btn-sm btn-block">Edit profile</a></div>
+                    <div class="media-body mb-5 text-white">
+                        <h4 class="mt-0 mb-0">Shrek</h4>
+                        <p class="small mb-4"> <i class="fas fa-map-marker-alt mr-2"></i>Swamp</p>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-light p-4 d-flex justify-content-end text-center">
+              <p>Maybe put search box here? </p>
+            </div>
+            <div class="px-4 py-3">
+                <h5 class="mb-0">About</h5>
+                <div class="p-4 rounded shadow-sm bg-light">
+                    <p class="font-italic mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+                        and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+   
+                </div>
+            </div>
+           
+        </div>
+
+<?php require APPROOT . '/views/includes/footer.php'; 
+?>
