617814f8d78fa5b52daff7f30e9f99771a6ce75d
Alvin Alagos <you@example.com>
2022-03-11 15:41:21 -0500

    completed home page prototype

--- Shufflewear/app/views/includes/footer.php
+++ Shufflewear/app/views/includes/footer.php
@@ -1,4 +1,13 @@
+<footer style=" 
+ background-image: linear-gradient(to right, black , orange);
+ color: white;
+   clear: left;
+">
+    <div class="bottom">
+        <h1>Test</h1>
+    </div>
 
+</footer>
 
 </body>
 
--- Shufflewear/app/views/includes/header.php
+++ Shufflewear/app/views/includes/header.php
@@ -5,9 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/header.css">
-    
-    <!-- <link rel="stylesheet" href="<?php echo URLROOT; ?>css/reset.css "> -->
-
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/Home.css">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
@@ -16,27 +14,17 @@
 
 <body>
 
-
+  <div class="black-bar"></div>
   <!-- Background image -->
-  <div class="p-5 text-center bg-image"
-  style=" background-image: url('https://images.unsplash.com/photo-1586396874197-b8fc676a5187?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80') !important;
-    height: 500px !important;
-    background-repeat: no-repeat !important;
-    background-size:cover !important;
-    filter:blur;"
-  >
-    <div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
-      <div class="d-flex justify-content-center align-items-center h-100">
+  <div class="p-5 bg-image"></div>
+    <div class="title" style="background-color: rgba(221, 221, 221, 0.7);">
         <div class="text-white">
-          <h1 class="mb-3">Heading</h1>
-          <h4 class="mb-3">Subheading</h4>
-          <a class="btn btn-outline-light btn-lg" href="#!" role="button"
-          >Call to action</a
-          >
+          <h1 class="mb-3">SHUFFLEWEAR</h1>
         </div>
-      </div>
     </div>
-  </div>
+
+    <div class="black-bar2"></div>
+  
   <!-- Background image -->
 
   <header>
@@ -56,7 +44,7 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarExample01">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item active">
+          <li class="nav-item ">
             <a class="nav-link" aria-current="page" href="#">Home</a>
           </li>
           <li class="nav-item">
