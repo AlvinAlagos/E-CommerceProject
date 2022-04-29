a1c26d53c8216c93f3f67bcad736d47056e62e02
Denmar Ermitano <ermitano.den@gmail.com>
2022-04-29 13:00:24 -0400

    to show only auctions with valid start/end dates

--- Shufflewear/app/views/Auction/auction.php
+++ Shufflewear/app/views/Auction/auction.php
@@ -8,31 +8,35 @@
 <div class="right-side" style="width: 85%; height:100%; float:left;">
     
     <div class="container">
-        <?php //dont forget to add the link in the thing later
+        <?php
             if (isset($data['auctions'])) {
-                foreach($data['auctions'] as $auction){
-                    echo 
-                    '<div style="border-bottom: 2px solid rgb(230,230,230);">
-                        <div class="my-5" style="height: 250px;">
-                            <div style="width: 30%; float: left; text-align: center;">
-                                <a href="/Shufflewear/Auction/description/' . $auction->auctionId . '">
-                                    <img src="' . URLROOT . '/public/img/' . $auction->img . '" style="object-fit: cover; height: 100%; border: 1px solid rgb(200,200,200); background-color: rgb(245,245,245);">
-                                </a>
-                            </div>
+                $currentDate = date('Y-m-d');
 
-                            <div style="width: 70%; float: right;">
-                                <h3 class="mb-5">
-                                    '. $auction->itemName .'
-                                </h3>
-                                <div>
-                                     '. ($auction->currentBid == 0 ? 'Starting at: $'. sprintf('%.2F', $auction->startingBid) : 'Current Bid: $'. sprintf('%.2F', $auction->currentBid)) .'
+                foreach($data['auctions'] as $auction){
+                    if ($auction->endDate >= $currentDate && $auction->endDate != date("0000-00-00", 0) && $auction->startDate != date("0000-00-00", 0))
+                        echo 
+                        '<div style="border-bottom: 2px solid rgb(230,230,230);">
+                            <div class="my-5" style="height: 250px;">
+                                <div style="width: 30%; float: left; text-align: center;">
+                                    <a href="/Shufflewear/Auction/description/' . $auction->auctionId . '">
+                                        <img src="' . URLROOT . '/public/img/' . $auction->img . '" style="object-fit: cover; height: 100%; border: 1px solid rgb(200,200,200); background-color: rgb(245,245,245);">
+                                    </a>
                                 </div>
-                                <div>
-                                    Available until: '. $auction->endDate .'
+
+                                <div style="width: 70%; float: right;">
+                                    <h3 class="mb-5">
+                                        '. $auction->itemName .'
+                                    </h3>
+                                    <div>
+                                            '. ($auction->currentBid == 0 ? 'Starting at: $'. sprintf('%.2F', $auction->startingBid) : 'Current Bid: $'. sprintf('%.2F', $auction->currentBid)) .'
+                                    </div>
+                                    <div>
+                                        Available until: '. $auction->endDate .'
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </div>';
+                        </div>';
+                    
                 }
             }
         ?>
