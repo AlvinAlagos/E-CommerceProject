3c6d00ee73e83b7132b016f3dc484d3d0d7b9416
Denmar Ermitano <ermitano.den@gmail.com>
2022-05-03 22:42:04 -0400

    fixed border to go all the way down

--- Shufflewear/app/views/Clothes/shop.php
+++ Shufflewear/app/views/Clothes/shop.php
@@ -2,36 +2,36 @@
 ?>
 
 <!-- <div class="container" style="width: 100%; height: 100%; margin-left:0; margin-right:-10px;"> -->
-<div class="filter-bar" style="width: 15%; height:100%; float:left; border-right: 2px solid black;">
+<div class="filter-bar" style="width: 15%; height: 100%; float:left; ">
 <h3 class="mb-3" style="margin-top:5%;margin-left:5%"><u>Filter</u></h3>
 
 <form method="post" action="" style="margin-left:5%;">
-  <p>Color:</p>
-  <input type="radio" id="Black" name="Color" value="Black">
-  <label for="Black">Black</label><br>
-  <input type="radio" id="Gray" name="Color"value="Gray">
-  <label for="Gray">Gray</label><br>
-  <input type="radio" id="White" name="Color"value="White">
-  <label for="White">White</label><br>
-  <input type="radio" id="Green" name="Color"value="Green">
-  <label for="Green">Green</label><br>
-  <input type="radio" id="Yellow" name="Color"value="Yellow">
-  <label for="Yellow">Yellow</label><br>
-  <input type="radio" id="Blue" name="Color"value="Blue">
-  <label for="Blue">Blue</label><br>
-  <input type="radio" id="Purple" name="Color"value="Purple">
-  <label for="Purple">Purple</label><br>
-  <input type="radio" id="Red" name="Color"value="Red">
-  <label for="Red">Red</label><br>
-  <input type="radio" id="Orange" name="Color"value="Orange">
-  <label for="Orange">Orange</label><br>
-  
-  <button class="btn btn-dark btn-lg btn-block" type="submit" name="filter" style="font-size:10px; width:50%;">Filter Color</button>   
+    <p>Color:</p>
+    <input type="radio" id="Black" name="Color" value="Black">
+    <label for="Black">Black</label><br>
+    <input type="radio" id="Gray" name="Color"value="Gray">
+    <label for="Gray">Gray</label><br>
+    <input type="radio" id="White" name="Color"value="White">
+    <label for="White">White</label><br>
+    <input type="radio" id="Green" name="Color"value="Green">
+    <label for="Green">Green</label><br>
+    <input type="radio" id="Yellow" name="Color"value="Yellow">
+    <label for="Yellow">Yellow</label><br>
+    <input type="radio" id="Blue" name="Color"value="Blue">
+    <label for="Blue">Blue</label><br>
+    <input type="radio" id="Purple" name="Color"value="Purple">
+    <label for="Purple">Purple</label><br>
+    <input type="radio" id="Red" name="Color"value="Red">
+    <label for="Red">Red</label><br>
+    <input type="radio" id="Orange" name="Color"value="Orange">
+    <label for="Orange">Orange</label><br>
+    
+    <button class="btn btn-dark btn-lg btn-block" type="submit" name="filter" style="font-size:10px; width:50%;">Filter Color</button>   
 
 </form>
 </div>
 
-<div class="right-side" style="width: 85%; height:100%; float:left;">
+<div class="right-side" style="width: 85%; height: auto; float:left; border-left: 2px solid black; ">
 
     <div class="container" style="margin-top: 5%;">
         <div class="row row-cols-3"> 
@@ -49,7 +49,7 @@
                             <h5 style="
                             font-style: normal;
                             font-weight: 300;
-                            font-size: 15px;">' . $listing->price . '</h5>';
+                            font-size: 15px;">' . sprintf('$%.2F', $listing->price) . '</h5>';
                     echo '</div>';
                 }
             ?>
